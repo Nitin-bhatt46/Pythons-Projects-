8a4caaccb138632a07869f8e06841b19ba42b886
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2023-04-20 16:54:41 +0530

    Add files via upload

---
+++ speaker.py
@@ -0,0 +1,16 @@
+
+
+## for windows
+
+import os
+
+
+if __name__ == '__main__':
+
+    while True:
+           txt = str(input("Enter anything to pronounce it: "))
+           if txt == "q":
+                    print("Exited..")
+                    break
+           cmd = f"Powershell Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{txt}')"
+           os.system(cmd)
